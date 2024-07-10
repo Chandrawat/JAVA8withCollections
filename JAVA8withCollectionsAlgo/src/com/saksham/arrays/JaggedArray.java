083602a9adf8daca335df96d6d91998b7f9dd7ac
Saksham Chandrawat <saksham.mist@gmail.com>
2024-07-10 16:36:52 -0500

    jagged Array

--- JAVA8withCollectionsAlgo/src/com/saksham/arrays/JaggedArray.java
+++ JAVA8withCollectionsAlgo/src/com/saksham/arrays/JaggedArray.java
@@ -4,8 +4,7 @@ public class JaggedArray {
 
 	public static void main(String[] args) {
 		
-		int[][] arr = new int[3][4];
-		
+		int[][] arr = new int[3][4];		
 		arr[0][0] = 1;
 		arr[0][1] = 2;
 		arr[0][2] = 3;
@@ -16,10 +15,37 @@ public class JaggedArray {
 		}
 		
 		int[] numbers = new int[] {3,5,2,0};
+		 
+		for(int i=0;i<numbers.length;i++) {
+			System.out.println("number is "+ numbers[i]);
+		}
 		
+		String[][] words= {
+				{"one","two","three"},
+				{"1","2","3"},
+				{"I","II","III"}
+		}; 
+		System.out.println("Words are");
+		for(int i=0;i<words.length;i++) {
+			for(int j=0;j<words[i].length;j++) {
+				System.out.println("i = "+ i + " j = "+j + " is " +words[i][j]);
+			}
+			System.out.println("-----");	
+		}
+	
+		//Jagged Array
 		
+		String[][] jaggedWords = {
+				{"1","2"},
+				{"A"},
+				{"one", "two"}
+		}; 
 		
-		
+		for(int i=0;i<jaggedWords.length;i++) {
+			for(int j=0;j<jaggedWords[i].length;j++) {
+				System.out.println("i = "+ i + " j = "+j + " is " +jaggedWords[i][j]);
+			}
+			System.out.println("------");
+		}
 	}
-
 }
