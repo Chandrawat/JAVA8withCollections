2faed11087f580ffe8c4b92929319176a5871c5f
Saksham Chandrawat <saksham.mist@gmail.com>
2024-07-10 16:12:56 -0500

    array one dimensional

--- JAVA8withCollectionsAlgo/src/com/saksham/arrays/ArrayP1.java
+++ JAVA8withCollectionsAlgo/src/com/saksham/arrays/ArrayP1.java
@@ -3,7 +3,20 @@ package com.saksham.arrays;
 public class ArrayP1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int[] intArray = new int[7];
+		
+		intArray[0] = 10;
+		intArray[1] = 20;
+		intArray[2] = 20;
+		intArray[3] = 30;
+		intArray[4] = 40;
+		intArray[5] = 20;
+		intArray[6] = 50;
+		
+		for(int i= 0; i<intArray.length; i++) {
+			System.out.println(intArray[i]);
+		}
 
 	}
 
---
+++ JAVA8withCollectionsAlgo/src/com/saksham/arrays/JaggedArray.java
@@ -0,0 +1,25 @@
+package com.saksham.arrays;
+
+public class JaggedArray {
+
+	public static void main(String[] args) {
+		
+		int[][] arr = new int[3][4];
+		
+		arr[0][0] = 1;
+		arr[0][1] = 2;
+		arr[0][2] = 3;
+		arr[0][3] = 4;
+		
+		for(int i=0; i<=arr.length;i++) {
+			System.out.println(arr[0][i]);
+		}
+		
+		int[] numbers = new int[] {3,5,2,0};
+		
+		
+		
+		
+	}
+
+}
